c9951cdf54a86ad59b5b5c6a4bfa384396e649c4
Adri4norec <asantana@reconcavo.org.br>
2026-04-17 17:05:09 -0300

    remover tela de registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { EquipamentComponent } from './equipament/equipament.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component'; 
 import { UserComponent } from './user/user.component';
 import { CadastroUserComponent } from './cadastro-user/cadastro-user.component';
 import { MovementComponent } from './movement/movement.component';
@@ -12,7 +11,6 @@ export const routes: Routes = [
   // Rotas de Autenticação
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }, 
 
   // Rotas de Gestão de Usuários
   { path: 'users', component: UserComponent },
